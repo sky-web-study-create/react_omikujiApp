24ae8d8e01e7f18af27eafc74ccb6ba7b80ac3e1
sky-web-study-create <sky.webstudy@gmail.com>
2022-04-03 19:33:10 +0900

    処理にコメントを入れる

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
-import { useState } from "react";
+import {useState} from "react";
 
 export const App = () => {
 
+  //ボタンの初期値を?に設定。クリックで状態変われば更新
   const [result, setResult] = useState("?");
+  //おみくじ結果の配列
   const omikujiArray = ["大吉", "中吉", "吉", "凶"];
 
   const onClickButton = () => {
